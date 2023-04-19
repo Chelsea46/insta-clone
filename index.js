82839070cb39b892f8f556a7ee705188f6849ac1
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-04-19 10:26:43 +0100

    challenge complete

--- index.js
+++ index.js
@@ -28,6 +28,40 @@ const postDeets = document.getElementById('post-details');
 const comments = document.getElementById('comments');
 const replyBtn = document.getElementById('reply-btn');
 const replies = document.getElementById('replies');
+const viewComments = document.querySelector('.view-comments')
+let modalContent = document.querySelector('.modal-content');
+
+// modal
+likedBy.addEventListener('click', ()=> {
+    document.querySelector('.bg-modal').style.display = 'flex';
+    modalContent.style.display = 'flex';
+    modalContent.innerHTML = `
+    <div class="close">+</div>
+    <div class="liked-user">
+        <img src="${data[1].img}" alt="cat" class="liked-img">
+        <p class="liked-name">${data[1].name}</p>
+    </div>
+    <div class="liked-user">
+        <img src="${data[2].img}" alt="cat" class="liked-img">
+        <p class="liked-name">${data[2].name}</p>
+    </div>
+    <div class="liked-user">
+        <img src="${data[3].img}" alt="cat" class="liked-img">
+        <p class="liked-name">${data[3].name}</p>
+</div>
+    `
+  });
+  
+document.addEventListener('click', function(e){
+    const target = e.target.closest('.close');
+    if(target){
+        document.querySelector('.bg-modal').style.display = 'none';
+        modalContent.style.display = 'none';
+        modalContent.innerHTML = ``
+    };
+});
+// end of modal
+
 
 // rendering to HTML
 nav.innerHTML = `<div class="logo">Instagram</div>
@@ -70,21 +104,39 @@ post.innerHTML = `      <header>
 </ul>
 </div>`;
 
-likedBy.innerHTML = `<p class="liked-by">20 Likes</p>`;
+likedBy.innerHTML = `<p class="liked-by">3 Likes</p>`;
 
 postDeets.innerHTML = ` <p class="user-name">${data[0].name}</p>
 <p class="user-cap">Lorem, ipsum dolor sit amet consectetur.</p>`;
 
-comments.innerHTML = `<p class="user-name">${data[1].name}</p>
-<div class="comment">
-    <p>${data[1].comment}</p>
-    <p class="comment-heart"><i class="fa-sharp fa-regular fa-heart " style="color: #ffffff;"></i></p>
-</div>`;
+// comments.innerHTML = `<p class="user-name">${data[1].name}</p>
+// <div class="comment">
+//     <p>${data[1].comment}</p>
+//     <p class="comment-heart"><i class="fa-sharp fa-regular fa-heart " style="color: #ffffff;"></i></p>
+// </div>`;
 
-replyBtn.innerHTML = `<button class="reply-btn">View replies</button>`;
+// replyBtn.innerHTML = `<button class="reply-btn">View replies</button>`;
 
-replies.innerHTML = `<p class="user-name">${data[2].name}</p>
-<p class="user-cap">${data[2].comment}</p>`;
+// replies.innerHTML = `<p class="user-name">${data[2].name}</p>
+// <p class="user-cap">${data[2].comment}</p>`;
+
+viewComments.addEventListener('click', () => {
+    viewComments.style.marginBottom = '0em';
+    comments.innerHTML = 
+    `<p class="user-name">${data[1].name}</p>
+    <div class="comment">
+        <p>${data[1].comment}</p>
+        <p class="comment-heart"><i class="fa-sharp fa-regular fa-heart " style="color: #ffffff;"></i></p>
+    </div>`;
+
+    replyBtn.innerHTML = 
+    `<button class="reply-btn">View replies</button>`;
+
+    replies.innerHTML = 
+    `<p class="user-name">${data[2].name}</p>
+    <p class="user-cap">${data[2].comment}</p>`;
+
+});
 
 // end of render
 
